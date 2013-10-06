77087c51e2bd653a5c6c9c6539211f030e134e3c
techXplored <techxplored@gmail.com>
2013-10-06 12:54:22 -0400

    te_time_ago rewritten

--- lib/init.php
+++ lib/init.php
@@ -318,20 +318,52 @@ function te_setup() {
 	/**
 	 * Post Time Format
 	 */
-	function te_time_ago() {
-		global $post;
 
-		$date = $post->post_date;
-		$time = get_post_time('G', true, $post);
-		$mytime = time() - $time;
-
-		if($mytime > 0 && $mytime < 7*24*60*60)
-		  $mytimestamp = sprintf(__('%s ago'), human_time_diff($time));
-		else
-		  $mytimestamp = date(get_option('date_format'), strtotime($date));
-		return $mytimestamp;
+	function te_time_ago_action() {
+		do_action('te_time_ago');
 	}
-	add_filter('the_time', 'te_time_ago');
+	function te_time_ago($from, $to = '') {
+		if ( empty( $to ) )
+			$to = time();
+
+		$diff = (int) abs( $to - $from );
+
+		if ( $diff < HOUR_IN_SECONDS ) {
+			$mins = round( $diff / MINUTE_IN_SECONDS );
+			if ( $mins <= 1 )
+				$mins = 1;
+			/* translators: min=minute */
+			$since = sprintf( _n( '%s minute', '%s minutes', $mins ), $mins );
+		} elseif ( $diff < DAY_IN_SECONDS && $diff >= HOUR_IN_SECONDS ) {
+			$hours = round( $diff / HOUR_IN_SECONDS );
+			if ( $hours <= 1 )
+				$hours = 1;
+			$since = sprintf( _n( '%s hour', '%s hours', $hours ), $hours );
+		} elseif ( $diff < WEEK_IN_SECONDS && $diff >= DAY_IN_SECONDS ) {
+			$days = round( $diff / DAY_IN_SECONDS );
+			if ( $days <= 1 )
+				$days = 1;
+			$since = sprintf( _n( '%s day', '%s days', $days ), $days );
+		} elseif ( $diff < 30 * DAY_IN_SECONDS && $diff >= WEEK_IN_SECONDS ) {
+			$weeks = round( $diff / WEEK_IN_SECONDS );
+			if ( $weeks <= 1 )
+				$weeks = 1;
+			$since = sprintf( _n( '%s week', '%s weeks', $weeks ), $weeks );
+		} elseif ( $diff < YEAR_IN_SECONDS && $diff >= 30 * DAY_IN_SECONDS ) {
+			$months = round( $diff / ( 30 * DAY_IN_SECONDS ) );
+			if ( $months <= 1 )
+				$months = 1;
+			$since = sprintf( _n( '%s month', '%s months', $months ), $months );
+		} elseif ( $diff >= YEAR_IN_SECONDS ) {
+			$years = round( $diff / YEAR_IN_SECONDS );
+			if ( $years <= 1 )
+				$years = 1;
+			$since = sprintf( _n( '%s year', '%s years', $years ), $years );
+		}
+
+		return $since;
+	}
+	add_action('te_time_ago_action', 'te_time_ago');
 
 	function te_share() {
 		do_action('te_share');
